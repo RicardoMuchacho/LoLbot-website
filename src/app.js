7d84bcc1186790888889f4db13bb7b979ec2bc22
rickmuch <ricardojrmuchacho@gmail.com>
2022-08-06 15:45:38 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+export class App {
+  constructor() {}
+
+  navGithub() {
+    window.open("https://github.com/RicardoMuchacho/LoL-discordBot");
+  }
+
+  inviteBot() {
+    window.open(
+      "https://discord.com/oauth2/authorize?client_id=1002349680388223139&permissions=2147493888&scope=bot%20applications.commands"
+    );
+  }
+}
